1eccd520e0241574e3266cc3988b46f0b5c22bd5
Zac Oldroyd <137462136+zoldroyd@users.noreply.github.com>
2025-01-17 19:25:59 -0700

    done

--- Mission2Ass/Program.cs
+++ Mission2Ass/Program.cs
@@ -1,3 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Mission2Ass;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to the Dice Throwing Simulator!");
+
+int numRolls = 0; // create numRolls
+
+Console.WriteLine("How many dice rolls would you like to simulate?");
+numRolls = int.Parse(Console.ReadLine()); // get the input
+
+int[] rollResults = // assign result array to roll results
+    diceSim.SimulateRolls(numRolls); // call the SimulateRolls method and pass numRolls
+int[] rollPercentage =
+    rollResults.Select(x => x * 100 / numRolls).ToArray(); // calculate the percentage of each roll
+for (int i = 2; i < rollPercentage.Length; i++) // loop through the rollPercentage array
+{
+    string
+        percentage =
+            new string('*', rollPercentage[i]); // create a string of '*' characters based on the percentage
+    Console.WriteLine(i + ": " + percentage); // display the number and the percentage
+}
+
+
+
+
+
---
+++ Mission2Ass/diceSim.cs
@@ -0,0 +1,20 @@
+namespace Mission2Ass;
+using System;
+public static class diceSim // create a static class for the dice roll simulator
+{
+    public static int[] SimulateRolls(int numRolls) // method to simulate dice rolls
+    {
+        Random random = new Random(); // create a new instance of the Random class
+
+        int[] rolls = new int[13]; // create an array to store the results
+
+        for (int i = 0; i < numRolls; i++) // loop through the number of die rolls
+        {
+            int tempRoll1 = random.Next(1, 7);// get a random number between 1 and 6 for the first die
+            int tempRoll2 = random.Next(1, 7);// get a random number between 1 and 6 for the second die
+            int rollSum = tempRoll1 + tempRoll2; // calculate the sum of the two dice rolls
+            rolls[rollSum]++; // increment the corresponding index in the rolls array
+        }
+        return rolls; // return the array of roll results to the main program
+    }
+}
